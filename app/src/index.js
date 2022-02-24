179cd3633220d1ba2b5ef49f7c68369e5973ed1d
benoit <benoit@assetik.com>
2022-02-24 15:25:23 +0400

    update

--- app/src/index.js
+++ app/src/index.js
@@ -6,8 +6,10 @@ const App = {
   account: null,
   meta: null,
 
-  start: async function() {
-    const { web3 } = this;
+  start: async function () {
+    const {
+      web3
+    } = this;
 
     try {
       // get contract instance
@@ -18,45 +20,73 @@ const App = {
         deployedNetwork.address,
       );
 
-      // get accounts
+      console.log('deployedNetwork.address: ' + deployedNetwork.address)
+
+      console.log('web3 version: ' + web3.version)
+
       const accounts = await web3.eth.getAccounts();
       this.account = accounts[0];
+      console.log('this.account: ' + this.account)
+
     } catch (error) {
       console.error("Could not connect to contract or chain.");
     }
   },
 
-  setStatus: function(message) {
+  setStatus: function (message) {
     const status = document.getElementById("status");
     status.innerHTML = message;
   },
 
-  createStar: async function() {
-    const { createStar } = this.meta.methods;
+  createStar: async function () {
+    const {
+      createStar
+    } = this.meta.methods;
     const name = document.getElementById("starName").value;
-    const id = document.getElementById("starId").value;
-    await createStar(name, id).send({from: this.account});
+    let id = document.getElementById("starId").value;
+    id = parseInt(id);
+    console.log('name:' + name)
+    console.log('typeof name:' + typeof name)
+    console.log('id:' + id)
+    console.log('typeof id:' + typeof id)
+    console.log('this.account:' + this.account)
+    console.log('createStar: ' + createStar)
+    await createStar(name, id).send({
+      from: this.account
+    });
+    console.log('start created')
     App.setStatus("New Star Owner is " + this.account + ".");
   },
 
   // Implement Task 4 Modify the front end of the DAPP
-  lookUp: async function (){
-    
+  lookUp: async function () {
+    const {
+      lookUptokenIdToStarInfo
+    } = this.meta.methods;
+    let id = document.getElementById("lookId").value;
+    id = parseInt(id);
+    console.log('id: ' + id)
+    console.log('lookUptokenIdToStarInfo:' + lookUptokenIdToStarInfo)
+
+    let info = await lookUptokenIdToStarInfo(id).call();
+
+    console.log('info: ' + info)
+    App.setStatus("Star info: " + info + ".");
   }
 
 };
 
 window.App = App;
 
-window.addEventListener("load", async function() {
+window.addEventListener("load", async function () {
   if (window.ethereum) {
     // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
     await window.ethereum.enable(); // get permission to access accounts
   } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
+    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live", );
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
+    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"), );
   }
 
   App.start();
--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,11 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+ const HDWalletProvider = require('truffle-hdwallet-provider');
+ 
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const infuraKey = fs.readFileSync(".infura-key").toString().trim();
 
 module.exports = {
   /**
@@ -50,6 +50,21 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 9545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+    },
+
+    ganache_client_test: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      gas: 0
+    },
+
+    rinkeby: {
+      networkCheckTimeout: 10000,
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4, // rinkeby's id
+      gas: 4500000, // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000
     },
 
     // Another network with more advanced options...
@@ -83,7 +98,7 @@ module.exports = {
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
-    // timeout: 100000
+    timeout: 100000
   },
 
   // Configure your compilers
